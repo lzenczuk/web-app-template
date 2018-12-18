3c51a1df1f977ca95effea808dbb96d30b4b3299
Lucjan Zenczuk <lucjan.zenczuk@gmail.com>
2018-12-18 16:18:09 +0000

    Add ace editor

---
+++ notes.md
@@ -0,0 +1,3 @@
+Good source of Ace editor configuration is on [https://ace.c9.io/build/kitchen-sink.html](https://ace.c9.io/build/kitchen-sink.html)
+
+Ace react FAQ [https://github.com/securingsincity/react-ace/blob/master/docs/FAQ.md](https://github.com/securingsincity/react-ace/blob/master/docs/FAQ.md)
--- src/index.js
+++ src/index.js
@@ -13,10 +13,20 @@ import { Provider } from 'react-redux'
 import store from "./config";
 
 import { FileManager } from "./containers/FileManager";
+import Grid from "@material-ui/core/Grid/Grid";
 
-
+import AceEditor from 'react-ace';
+import 'brace/mode/c_cpp';
+import 'brace/snippets/c_cpp';
+import 'brace/theme/textmate';
+import 'brace/ext/language_tools';
 
 const App = () => {
+
+    const onChange = (newValue) => {
+        console.log("----------> Editor: "+newValue)
+    };
+
     return <Provider store={store}>
         <Fragment>
             <CssBaseline/>
@@ -26,11 +36,27 @@ const App = () => {
                     <Button>Login</Button>
                 </ToolBar>
             </AppBar>
-            <div style={{marginTop: 48, padding: 8, display: "flex"}}>
-                <Paper>
-                    <FileManager/>
-                </Paper>
-            </div>
+            <Grid container direction="row" justify="flex-start" alignItems="stretch" spacing={8}>
+                <Grid item xs={3} container alignItems="stretch">
+                    <Paper style={{flexGrow: 1}}>
+                        <FileManager/>
+                    </Paper>
+                </Grid>
+                <Grid item xs={9} container alignItems="stretch">
+                        <AceEditor
+                            mode="c_cpp"
+                            theme="textmate"
+                            name="UNIQUE_ID_OF_DIV"
+                            value={"Test, test, test"}
+                            onChange={onChange}
+                            editorProps={{$blockScrolling: true}}
+                            enableBasicAutocompletion={true}
+                            enableLiveAutocompletion={true}
+                            enableSnippets={true}
+                            style={{flexGrow: 1, height: "100%"}}
+                        />
+                </Grid>
+            </Grid>
         </Fragment>
     </Provider>
 };
