d8adbe9079166bd94d3e9e9fbea9b5a21e395a55
Lucjan Zenczuk <lucjan.zenczuk@gmail.com>
2019-02-15 15:59:08 +0000

    Add send but now work as expected

--- src/modules/projectIDE/api.js
+++ src/modules/projectIDE/api.js
@@ -1,25 +1,67 @@
 import {
-    arrayBufferToPrintableString, projectToArrayBuffer,
-    textFileToArrayBuffer,
-    textToArrayBuffer
+    arrayBufferToPrintableString, projectToArrayBuffer, textToArrayBuffer,
 } from "./projectModelBinarySerialization";
-import {createFile, createFolder, createProject} from "./projectModel";
+import ecc from 'eosjs-ecc'
+import ScatterJS from "scatterjs-core";
 
-export const sendProjectToServer = (project) => {
+const splitStringTo12CharsChunks = (stringToSplit) => {
+    let startIndex = 0;
+    let endIndex = 12;
 
-    let arrayBuffer = projectToArrayBuffer(project);
+    const chunks = [];
 
-    console.log("-------> Buffer: "+arrayBufferToPrintableString(arrayBuffer));
+    while (true) {
+        if (startIndex > stringToSplit.length - 1) return chunks;
+        chunks.push(stringToSplit.substring(startIndex, endIndex));
+        startIndex = startIndex + 12;
+        endIndex = endIndex + 12;
+    }
+};
 
-    fetch("/server", {
-        method: "POST",
-        cache: "no-cache",
-        body: new Blob([new Uint8Array(arrayBuffer)])
-    }).then(response => {
-        console.log("-----> server response"+response)
-    }).catch(error => {
-        console.error(error)
+const readChunk = (reader, readerFunction) => {
+    reader.read().then(({done, value}) => {
+        if(done){
+            return;
+        }
+
+        readerFunction(value);
+        return readChunk(reader, readerFunction)
     })
+};
+
+export const sendProjectToServer = (project, publicKey) => {
+
+    let fileBuffer = projectToArrayBuffer(project);
+    let publicKeyBuffer = textToArrayBuffer(publicKey);
+
+    let hashSha256 = ecc.sha256(fileBuffer, 'hex');
+    let hashSha256AsWords = splitStringTo12CharsChunks(hashSha256).join(' ');
+
+    ScatterJS.scatter.getArbitrarySignature(publicKey, hashSha256AsWords, 'Project file signature', true).then(signature => {
+
+        console.log("Signature: " + signature);
+
+        fetch("/server", {
+            method: "POST",
+            cache: "no-cache",
+            body: new Blob([new Uint8Array(fileBuffer)])
+        }).then(response => {
+            console.log("-----> server response" + response);
+            return response.body
+        }).then(body => {
+            const reader = body.getReader();
+
+            return readChunk(reader, (v) => console.log("=======> receive: "+v))
+
+        }).catch(error => {
+            console.error(error)
+        })
+
+
+    }).catch(error => {
+        console.log("---------> Error!", error)
+    });
+
 };
 
 
